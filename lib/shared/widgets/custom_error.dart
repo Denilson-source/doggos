1c1e88fc5e883c4f623b6606a7d39debd9c4f497
Denilson Amaral <denilson19282@gmail.com>
2021-06-07 19:47:37 -0300

    add custom error widget

---
+++ lib/shared/widgets/custom_error.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class CustomError extends StatelessWidget {
+  final String error;
+  final void Function()? action;
+  final String? actionLabel;
+
+  CustomError({required this.error})
+      : this.action = null,
+        this.actionLabel = null;
+
+  CustomError.withAction({
+    required this.error,
+    required this.actionLabel,
+    required this.action,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(error),
+          if (action != null) ...[
+            SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: action,
+              child: Text(actionLabel!),
+            ),
+          ]
+        ],
+      ),
+    );
+  }
+}
